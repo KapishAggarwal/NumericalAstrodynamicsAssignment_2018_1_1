59f9c8c04ba39c7400cfd5c67ef8b747b0aa0c38
Dominic Dirkx <d.dirkx@tudelft.nl>
2018-12-07 16:13:51 +0100

    Changes for Q3

--- src3/delfiVariationalEquations.cpp
+++ src3/delfiVariationalEquations.cpp
@@ -65,19 +65,19 @@ int main()
     // Set simulation time settings.
     double simulationStartEpoch = tudat::physical_constants::JULIAN_YEAR;
 
-    // **** MODIFY FOR AE4867: define list of bodies required for simulation
+    // **** MODIFY FOR AE4868: define list of bodies required for simulation
     double simulationEndEpoch;// = ...
 
     // Define body settings for simulation.
     std::vector< std::string > bodiesToCreate;
-    // **** MODIFY FOR AE4867: define list of bodies required for simulation
 
-    // Create body objects.
+    // Set Earth gravity field settings.
     std::map< std::string, std::shared_ptr< BodySettings > > bodySettings =
             getDefaultBodySettings( bodiesToCreate, simulationStartEpoch - 300.0, simulationEndEpoch + 300.0 );
+    bodySettings[ "Earth" ]->gravityFieldSettings =
+            std::make_shared< FromFileSphericalHarmonicsGravityFieldSettings >( ggm02c );
 
-    // **** MODIFY FOR AE4867: update environment settings to comply with requirements
-
+    // Create body objects.
     NamedBodyMap bodyMap = createBodies( bodySettings );
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -86,16 +86,44 @@ int main()
 
     // Create spacecraft object.
     bodyMap[ "Delfi" ] = std::make_shared< simulation_setup::Body >( );
-
     bodyMap[ "Delfi" ]->setEphemeris( std::make_shared< TabulatedCartesianEphemeris< > >(
-                                            std::shared_ptr< interpolators::OneDimensionalInterpolator
-                                            < double, Eigen::Vector6d > >( ), "Earth", "ECLIPJ2000" ) );
+                                          std::shared_ptr< interpolators::OneDimensionalInterpolator
+                                          < double, Eigen::Vector6d > >( ), "Earth", "J2000" ) );
 
-    // **** MODIFY FOR AE4867: all required properties of body Delfi
+    // **** MODIFY FOR AE4868: all required properties of body Delfi
+    bodyMap[ "Delfi" ]->setConstantBodyMass( 3.5 );
 
+       bodyMap[ "Delfi" ]->setEphemeris( boost::make_shared< TabulatedCartesianEphemeris< > >(
+                                             boost::shared_ptr< interpolators::OneDimensionalInterpolator
+                                             < double, Eigen::Vector6d > >( ), "Earth", "J2000" ) );
+
+       // Create aerodynamic coefficient interface settings.
+       double referenceArea = 0.02;
+       double aerodynamicCoefficient = 1.5;
+       boost::shared_ptr< AerodynamicCoefficientSettings > aerodynamicCoefficientSettings =
+               boost::make_shared< ConstantAerodynamicCoefficientSettings >(
+                   referenceArea, aerodynamicCoefficient * Eigen::Vector3d::UnitX( ), 1, 1 );
+
+       // Create and set aerodynamic coefficients object
+       bodyMap[ "Delfi" ]->setAerodynamicCoefficientInterface(
+                   createAerodynamicCoefficientInterface( aerodynamicCoefficientSettings, "Delfi" ) );
+
+       // Create radiation pressure settings
+       double referenceAreaRadiation = 0.02;
+       double radiationPressureCoefficient = 1.2;
+       std::vector< std::string > occultingBodies;
+       occultingBodies.push_back( "Earth" );
+       boost::shared_ptr< RadiationPressureInterfaceSettings > delfiRadiationPressureSettings =
+               boost::make_shared< CannonBallRadiationPressureInterfaceSettings >(
+                   "Sun", referenceAreaRadiation, radiationPressureCoefficient, occultingBodies );
+
+       // Create and set radiation pressure settings
+       bodyMap[ "Delfi" ]->setRadiationPressureInterface(
+                   "Sun", createRadiationPressureInterface(
+                       delfiRadiationPressureSettings, "Delfi", bodyMap ) );
 
     // Finalize body creation.
-    setGlobalFrameBodyEphemerides( bodyMap, "SSB", "ECLIPJ2000" );
+    setGlobalFrameBodyEphemerides( bodyMap, "SSB", "J2000" );
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     ///////////////////////            CREATE ACCELERATIONS          //////////////////////////////////////////////////////
@@ -107,11 +135,8 @@ int main()
     std::vector< std::string > centralBodies;
     centralBodies.push_back( "Earth" );
 
-    // **** MODIFY FOR AE4867: define settings for accelerations acting on Delfi.
-
+    // **** MODIFY FOR AE4868: define settings for accelerations acting on Delfi.
     std::map< std::string, std::vector< std::shared_ptr< AccelerationSettings > > > accelerationsOfDelfi;
-
-
 
 
     SelectedAccelerationMap accelerationMap;
@@ -124,31 +149,28 @@ int main()
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // Set Keplerian elements for Delfi.
-
-    // **** MODIFY FOR AE4867: set initial state according to your student number
-
     Eigen::Vector6d delfiInitialStateInKeplerianElements;
     delfiInitialStateInKeplerianElements( semiMajorAxisIndex ) = 7500.0E3;
     delfiInitialStateInKeplerianElements( eccentricityIndex ) = 0.1;
     delfiInitialStateInKeplerianElements( inclinationIndex ) = unit_conversions::convertDegreesToRadians( 85.3 );
-//    delfiInitialStateInKeplerianElements( argumentOfPeriapsisIndex ) = 0.0;
-//    delfiInitialStateInKeplerianElements( longitudeOfAscendingNodeIndex ) = 0.0;
-//    delfiInitialStateInKeplerianElements( trueAnomalyIndex ) = 0.0;
 
+    // **** MODIFY FOR AE4868: set initial state according to your student number
+    //    delfiInitialStateInKeplerianElements( argumentOfPeriapsisIndex ) = 0.0;
+    //    delfiInitialStateInKeplerianElements( longitudeOfAscendingNodeIndex ) = 0.0;
+    //    delfiInitialStateInKeplerianElements( trueAnomalyIndex ) = 0.0;
+
+    // Compute initial Cartesian state
     double earthGravitationalParameter = bodyMap.at( "Earth" )->getGravityFieldModel( )->getGravitationalParameter( );
     const Eigen::Vector6d delfiInitialState = convertKeplerianToCartesianElements(
                 delfiInitialStateInKeplerianElements, earthGravitationalParameter );
 
-
-
+    // Create propagator settings
     std::shared_ptr< TranslationalStatePropagatorSettings< double > > propagatorSettings;
     propagatorSettings = std::make_shared< TranslationalStatePropagatorSettings< double > >(
                 centralBodies, accelerationModelMap, bodiesToPropagate, delfiInitialState, simulationEndEpoch );
 
-    // **** MODIFY FOR AE4867: create integrator settings.
-
+    // **** MODIFY FOR AE4868: create integrator settings.
     std::shared_ptr< IntegratorSettings< > > integratorSettings;
-
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     ///////////////////////    DEFINE PARAMETERS FOR WHICH SENSITIVITY IS TO BE COMPUTED   ////////////////////////////////
@@ -156,8 +178,7 @@ int main()
 
     // Define list of parameters to estimate.
     std::vector< std::shared_ptr< EstimatableParameterSettings > > parameterNames;
-
-    // **** MODIFY FOR AE4867, question 3.2: define list of parameters for which state transition/sensitivity matrix are to be propagated.
+    // **** MODIFY FOR AE4868, question 2: define list of parameters for which state transition/sensitivity matrix are to be propagated.
 
 
     // Create parameters
@@ -167,58 +188,60 @@ int main()
     // Print identifiers and indices of parameters to terminal.
     printEstimatableParameterEntries( parametersToEstimate );
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ///////////////////////             PROPAGATE ORBIT AND VARIATIONAL EQUATIONS         /////////////////////////////////
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // **** MODIFY FOR AE4867, question 3.1: propagate equations of motion
-    // **** MODIFY FOR AE4867, question 3.2: propagate equations of motion and variational equations
-
-
-    std::map< double, Eigen::MatrixXd > stateTransitionResult;
-    std::map< double, Eigen::MatrixXd > sensitivityResult;
-    std::map< double, Eigen::VectorXd > nominalIntegrationResult;
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ///////////////////////        PROVIDE OUTPUT TO CONSOLE AND FILES           //////////////////////////////////////////
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    std::string outputSubFolder = "DelfiVariationalEquations/";
-
-    // Write perturbed satellite propagation history to file.
-    input_output::writeDataMapToTextFile( nominalIntegrationResult,
-                                          "singlePerturbedSatellitePropagationHistory.dat",
-                                          getOutputPath( ) + outputSubFolder,
-                                          "",
-                                          std::numeric_limits< double >::digits10,
-                                          std::numeric_limits< double >::digits10,
-                                          "," );
-
-    input_output::writeDataMapToTextFile( stateTransitionResult,
-                                          "singlePerturbedSatelliteStateTransitionHistory.dat",
-                                          getOutputPath( ) + outputSubFolder,
-                                          "",
-                                          std::numeric_limits< double >::digits10,
-                                          std::numeric_limits< double >::digits10,
-                                          "," );
-
-    input_output::writeDataMapToTextFile( sensitivityResult,
-                                          "singlePerturbedSatelliteSensitivityHistory.dat",
-                                          getOutputPath( ) + outputSubFolder,
-                                          "",
-                                          std::numeric_limits< double >::digits10,
-                                          std::numeric_limits< double >::digits10,
-                                          "," );
-
-    Eigen::Vector6d originalInitialState = delfiInitialState;
-
-    // Iterate over all entries of initial state
-    for( unsigned int entry = 0; entry < 6; entry++ )
+    for( unsigned int question = 1; question <= 3; question++ )
     {
-        // **** MODIFY FOR AE4867, question 3.3: propagate orbits needed to find limits of validity of linearization.
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////             PROPAGATE ORBIT AND VARIATIONAL EQUATIONS         /////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // **** MODIFY FOR AE4868, question 1: propagate equations of motion
+        // **** MODIFY FOR AE4868, question 2: propagate equations of motion and variational equations
+
+
+        std::map< double, Eigen::MatrixXd > stateTransitionResult;
+        std::map< double, Eigen::MatrixXd > sensitivityResult;
+        std::map< double, Eigen::VectorXd > nominalIntegrationResult;
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////        PROVIDE OUTPUT TO CONSOLE AND FILES           //////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        std::string outputSubFolder = "DelfiVariationalEquations/";
+
+        // Write perturbed satellite propagation history to file.
+        input_output::writeDataMapToTextFile( nominalIntegrationResult,
+                                              "singlePerturbedSatellitePropagationHistory.dat",
+                                              getOutputPath( ) + outputSubFolder,
+                                              "",
+                                              std::numeric_limits< double >::digits10,
+                                              std::numeric_limits< double >::digits10,
+                                              "," );
+
+        input_output::writeDataMapToTextFile( stateTransitionResult,
+                                              "singlePerturbedSatelliteStateTransitionHistory.dat",
+                                              getOutputPath( ) + outputSubFolder,
+                                              "",
+                                              std::numeric_limits< double >::digits10,
+                                              std::numeric_limits< double >::digits10,
+                                              "," );
+
+        input_output::writeDataMapToTextFile( sensitivityResult,
+                                              "singlePerturbedSatelliteSensitivityHistory.dat",
+                                              getOutputPath( ) + outputSubFolder,
+                                              "",
+                                              std::numeric_limits< double >::digits10,
+                                              std::numeric_limits< double >::digits10,
+                                              "," );
+
+        Eigen::Vector6d originalInitialState = delfiInitialState;
+
+        // Iterate over all entries of initial state
+        for( unsigned int entry = 0; entry < 6; entry++ )
+        {
+            // **** MODIFY FOR AE4868, question 3: propagate orbits needed to find limits of validity of linearization.
+        }
+
     }
-
-
     // Final statement.
     // The exit code EXIT_SUCCESS indicates that the program was successfully executed.
     return EXIT_SUCCESS;
